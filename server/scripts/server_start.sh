1cebe0963a9852dae77a70f8691eca03b663f82e
YunGeunSang <120304078+YunGeunSang@users.noreply.github.com>
2023-05-08 22:28:21 +0900

    feat: 파라미터 스토어 적용 v14

--- server/scripts/server_start.sh
+++ server/scripts/server_start.sh
@@ -1,7 +1,3 @@
 cd /home/ubuntu/build
 
-export JDBC_URL=$(aws ssm get-parameters --region ap-northeast-2 --names /config/seb43main029/jdbc.url --query Parameters[0].Value --output text)
-export JDBC_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --names /config/seb43main029/jdbc.username --query Parameters[0].Value --output text)
-export JDBC_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names /config/seb43main029/jdbc.password --query Parameters[0].Value --output text)
-
 sudo nohup java -jar seb43_main_029-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
